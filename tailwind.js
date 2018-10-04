787429aa53582e14baa520153dd0bd4335fbf55c
flemay <461102+flemay@users.noreply.github.com>
2018-10-04 21:55:06 +1000

    new colors

--- tailwind.js
+++ tailwind.js
@@ -47,6 +47,7 @@ let colors = {
   'transparent': 'transparent',
 
   'black': '#22292f',
+  'black-light': '#363636',
   'grey-darkest': '#3d4852',
   'grey-darker': '#606f7b',
   'grey-dark': '#8795a1',
@@ -55,6 +56,7 @@ let colors = {
   'grey-lighter': '#f1f5f8',
   'grey-lightest': '#f8fafc',
   'white': '#ffffff',
+  'white-dark': '#f5f5f5',
 
   'red-darkest': '#3b0d0c',
   'red-darker': '#621b18',
