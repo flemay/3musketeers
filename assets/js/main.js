725b5b815de0fcba7f0c6d15e4e65e746cc35ef6
flemay <461102+flemay@users.noreply.github.com>
2019-03-12 19:37:09 +1100

    clean up

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const navbarBurger = document.getElementById("navbar-burger");
-  const navbarLinksMenu = document.getElementById("nav-show-hide");
-  navbarBurger.addEventListener('click', () => {
-    navbarLinksMenu.classList.toggle('hidden');
-  });
-});
