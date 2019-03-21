42a226f8f8696366f6602fa0855ad06cf27ce5e2
flemay <461102+flemay@users.noreply.github.com>
2019-03-21 20:20:11 +1100

    center user image vertically

--- docs/.vuepress/components/Users.vue
+++ docs/.vuepress/components/Users.vue
@@ -2,10 +2,10 @@
   <div>
     <div class="users-container">
       <div class="user-container" v-for="user in data.users">
-        <div class="user-card">
+        <div class="img-container">
           <img v-bind:src="user.image" v-bind:alt="user.name">
-          <a v-bind:href="user.url">{{ user.name }}</a>
         </div>
+        <a v-bind:href="user.url">{{ user.name }}</a>
       </div>
     </div>
   </div>
@@ -22,6 +22,10 @@ export default {
 </script>
 
 <style lang="stylus">
+$user-container-height = 160px
+$user-container-width = 150px
+$img-container-height = 130px
+
 .users-container
   display flex
   flex-wrap wrap
@@ -30,9 +34,19 @@ export default {
   justify-content center
   margin-bottom 50px
   .user-container
+    display flex
     flex-direction column
     margin 25px
-    max-width 150px
-    .user-card
-      text-align center
+    justify-content flex-end
+    align-items center
+    height $user-container-height
+    width $user-container-width
+    .img-container
+      display flex
+      flex-direction column
+      height $img-container-height
+      justify-content center
+      img
+        max-height $img-container-height
+        max-width $user-container-width
 </style>
