a4951cfbb15f46b00b608e66301bf22ac2dcda81
flemay <461102+flemay@users.noreply.github.com>
2019-03-24 11:55:35 +1100

    improve favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,13 +9,23 @@ module.exports = {
   title: '3 Musketeers',
   description: desc,
   head: [
-    ['link', { rel: 'icon', href: logoRel }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/favicon/site.webmanifest' }],
+    ['link', { rel: 'mask-icon', href: '/favicon/safari-pinned-tab.svg', color: '#000000' }],
+    ['link', { rel: 'shortcut icon', href: '/favicon/favicon.ico' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+    ['meta', { name: 'msapplication-config', content: '/favicon/browserconfig.xml' }],
+    ['meta', { name: 'theme-color', content: '#000000' }],
+
     // facebook open graph tags
     ['meta', { property: 'og:url', content: url }],
     ['meta', { property: 'og:title', content: title }],
     ['meta', { property: 'og:description', content: desc }],
     ['meta', { property: 'og:site_name', content: domain }],
     ['meta', { property: 'og:image', content: logoRel }],
+
     //twitter card tags additive with the og: tags
     ['meta', { name: 'twitter:domain', value: domain }],
     ['meta', { name: 'twitter:title', value: title }],
