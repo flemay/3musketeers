9a8f2cb11ed919e65d92618a58fbecd54f932b21
flemay <461102+flemay@users.noreply.github.com>
2020-02-09 17:25:39 +1100

    update

--- docs/.vuepress/theme/components/PagePost.vue
+++ docs/.vuepress/theme/components/PagePost.vue
@@ -2,7 +2,7 @@
   <main class="page">
     <article class="theme-default-content">
       <h1>{{ this.$page.frontmatter.title }}</h1>
-      <Content  />
+      <Content />
       <br>
       <footer>
         <nav><span>←<a href="/blog/">Back</a></span></nav>
